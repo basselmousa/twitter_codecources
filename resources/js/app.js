a19ad175b117099bca60d6f8c0a45e67aa8a9722
bassel-mousa <--get>
2021-03-15 22:34:08 +0200

    installing pusher and laravel echo

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,6 +15,8 @@ Vue.use(Vuex);
 import VueObserveVisibility from 'vue-observe-visibility'
 Vue.use(VueObserveVisibility)
 
+Vue.prototype.$user = User;
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/api/timeline', [App\Http\Controllers\Api\Timeline\TimelineController::class, 'index']);
