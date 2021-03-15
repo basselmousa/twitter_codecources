1d430bc23fa54985925dad863765c4ae43e973b1
bassel-mousa <--get>
2021-03-15 22:37:47 +0200

    Adding A column to tweets table

--- database/migrations/2021_03_12_185005_create_tweets_table.php
+++ database/migrations/2021_03_12_185005_create_tweets_table.php
@@ -16,7 +16,7 @@ class CreateTweetsTable extends Migration
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->unsigned()->index();
-            $table->string('body', 250);
+            $table->text('body');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
