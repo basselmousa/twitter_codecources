fcb330a8a9c88fcfec0e3482c8658ec2ac5bb7f5
bassel-mousa <--get>
2021-03-16 19:19:06 +0200

    Change The body column to nullable

--- database/migrations/2021_03_12_185005_create_tweets_table.php
+++ database/migrations/2021_03_12_185005_create_tweets_table.php
@@ -16,7 +16,7 @@ class CreateTweetsTable extends Migration
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->unsigned()->index();
-            $table->text('body');
+            $table->text('body')->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
