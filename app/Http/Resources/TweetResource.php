c4b577721410c0b8f4757f7b8122377bd7777334
bassel-mousa <--get>
2021-03-16 19:16:25 +0200

    Initial The api resource

--- app/Http/Resources/TweetResource.php
+++ app/Http/Resources/TweetResource.php
@@ -17,6 +17,8 @@ class TweetResource extends JsonResource
         return [
             'id' => $this->id,
             'body' => $this->body,
+            'type'=>$this->type,
+            'original_tweet' => new TweetResource($this->originalTweet),
             'user' => new UserResource($this->user),
             'created_at' => $this->created_at->timestamp
         ];
