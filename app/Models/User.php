55e0fa7c7b983316896b5eadf74a812dee068cf5
bassel-mousa <--get>
2021-03-17 21:59:56 +0200

    Making Relationship between User And Like Tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,5 +72,10 @@ class User extends Authenticatable
     {
         return $this->hasManyThrough(Tweet::class, Follower::class, 'user_id', 'user_id', 'id', 'following_id');
     }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
 
