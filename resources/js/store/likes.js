38c94d3b62e0f05e05519dfb675d986f01be3aee
bassel-mousa <--get>
2021-03-17 21:57:06 +0200

    Making Vuex file for Likes on tweets

---
+++ resources/js/store/likes.js
@@ -0,0 +1,16 @@
+export  default {
+    namespaced:true,
+    state:{
+        likes:[],
+    },
+    getters:{
+      likes(state){
+          return state.likes
+      }
+    },
+    mutations:{
+        PUSH_LIKES(state, data){
+            state.likes.push(...data)
+        }
+    }
+}
