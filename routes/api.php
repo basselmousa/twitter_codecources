1446991d56c32735bd168b980af97c3feda2d16d
bassel-mousa <--get>
2021-03-18 22:44:26 +0200

    Making the api for storing and deleting the like

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/timeline', [App\Http\Controllers\Api\Timeline\TimelineController::class, 'index']);
 Route::post('/tweets', [\App\Http\Controllers\Api\Tweets\TweetsController::class, 'store']);
+Route::post('/tweets/{tweet}/likes', [\App\Http\Controllers\Api\Tweets\TweetLikeController::class, 'store']);
+Route::delete('/tweets/{tweet}/likes', [\App\Http\Controllers\Api\Tweets\TweetLikeController::class, 'destroy']);
