ded101159714c30b119a67fddceb38ed138792e2
bassel-mousa <--get>
2021-03-18 22:42:38 +0200

    Making The Likes Store With Vuex To Make it real time

--- resources/js/store/likes.js
+++ resources/js/store/likes.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export  default {
     namespaced:true,
     state:{
@@ -11,6 +13,14 @@ export  default {
     mutations:{
         PUSH_LIKES(state, data){
             state.likes.push(...data)
+        }
+    },
+    actions:{
+        async likeTweet(_, tweet){
+            await  axios.post(`api/tweets/${tweet.id}/likes`)
+        },
+        async unlikeTweet(_, tweet){
+            await  axios.delete(`api/tweets/${tweet.id}/likes`)
         }
     }
 }
