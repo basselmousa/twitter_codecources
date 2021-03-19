75db3252e72dfc6e213fdb56696344eb51e7b18c
bassel-mousa <--get>
2021-03-19 15:32:49 +0200

    Making Retweets Store

---
+++ resources/js/store/retweets.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import {without} from 'lodash'
+
+export default {
+    namespaced: true,
+    state: {
+        retweets: [],
+    },
+    getters: {
+        retweets(state) {
+            return state.retweets
+        }
+    },
+    mutations: {
+        PUSH_RETWEETS(state, data) {
+            state.retweets.push(...data)
+        },
+    },
+}
