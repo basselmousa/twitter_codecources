788f33864c6737f4c6b7bc7f97947ba0fb2edbcf
bassel-mousa <--get>
2021-03-19 23:01:04 +0200

    Making Some Changes to make sure the all data has retrived

--- app/Http/Resources/TweetCollection.php
+++ app/Http/Resources/TweetCollection.php
@@ -37,7 +37,10 @@ class TweetCollection extends ResourceCollection
             return [];
         }
         return $user->likes()
-            ->whereIn('tweet_id', $this->collection->pluck('id'))
+            ->whereIn('tweet_id',
+                $this->collection->pluck('id')
+                    ->merge($this->collection->pluck('original_tweet_id'))
+            )
             ->pluck('tweet_id')
             ->toArray();
     }
@@ -49,7 +52,10 @@ class TweetCollection extends ResourceCollection
             return [];
         }
         return $user->retweets()
-            ->whereIn('original_tweet_id', $this->collection->pluck('id'))
+            ->whereIn('original_tweet_id',
+                $this->collection->pluck('id')
+                    ->merge($this->collection->pluck('original_tweet_id'))
+            )
             ->pluck('original_tweet_id')
             ->toArray();
     }
