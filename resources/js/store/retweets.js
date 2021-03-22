e2ab207e30e16ba68a1d3dfdb23134bcf8d3d373
bassel-mousa <--get>
2021-03-22 23:32:55 +0200

    Initial The Actions

--- resources/js/store/retweets.js
+++ resources/js/store/retweets.js
@@ -16,4 +16,13 @@ export default {
             state.retweets.push(...data)
         },
     },
+    actions:{
+
+        async retweetTweet(_, tweet) {
+            await axios.post(`api/tweets/${tweet.id}/retweets`)
+        },
+        async unretweetTweet(_, tweet) {
+            await axios.delete(`api/tweets/${tweet.id}/retweets`)
+        },
+    }
 }
