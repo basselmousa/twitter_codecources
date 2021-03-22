a650b9f4bee0412432c4e4bb230201883f352d73
bassel-mousa <--get>
2021-03-22 23:32:29 +0200

    Initial The Routes to Retweet

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/timeline', [App\Http\Controllers\Api\Timeline\TimelineController::class, 'index']);
 Route::post('/tweets', [\App\Http\Controllers\Api\Tweets\TweetsController::class, 'store']);
+
 Route::post('/tweets/{tweet}/likes', [\App\Http\Controllers\Api\Tweets\TweetLikeController::class, 'store']);
 Route::delete('/tweets/{tweet}/likes', [\App\Http\Controllers\Api\Tweets\TweetLikeController::class, 'destroy']);
+
+Route::post('/tweets/{tweet}/retweets', [\App\Http\Controllers\Api\Tweets\TweetRetweetController::class, 'store']);
+Route::delete('/tweets/{tweet}/retweets', [\App\Http\Controllers\Api\Tweets\TweetRetweetController::class, 'destroy']);
