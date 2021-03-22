9c139b2e6f15e02f8e1e60ee11ffd6e400bdc2e1
bassel-mousa <--get>
2021-03-22 23:34:19 +0200

    Initial The Relationship That Lead To retweeted Tweet

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -29,4 +29,9 @@ class Tweet extends Model
     {
         return $this->hasMany(Tweet::class,'original_tweet_id');
     }
+
+    public function retweetedTweet()
+    {
+        return $this->hasOne(Tweet::class, 'original_tweet_id', 'id');
+    }
 }
