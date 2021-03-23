bc9a5d9375df94e67b701a27d8800d42f87b09c6
bassel-mousa <--get>
2021-03-24 00:30:13 +0200

    Making Events For Retweet And Delete Retweet And BroadCast

---
+++ app/Events/Tweets/TweetRetweetsWereUpdated.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Events\Tweets;
+
+use App\Models\Tweet;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TweetRetweetsWereUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    protected $user;
+    protected $tweet;
+    public function __construct(User $user, Tweet $tweet)
+    {
+        //
+        $this->user = $user;
+        $this->tweet = $tweet;
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'id' => $this->tweet->id,
+            'user_id' => $this->user->id,
+            'count' => $this->tweet->retweets->count(),
+        ];
+    }
+
+
+    public function broadcastAs()
+    {
+        return 'TweetRetweetsWereUpdated';
+    }
+
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('tweets');
+    }
+}
--- app/Http/Controllers/Api/Tweets/TweetRetweetController.php
+++ app/Http/Controllers/Api/Tweets/TweetRetweetController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Api\Tweets;
 
+use App\Events\Tweets\TweetRetweetsWereUpdated;
+use App\Events\Tweets\TweetWasCreated;
+use App\Events\Tweets\TweetWasDeleted;
 use App\Http\Controllers\Controller;
 use App\Models\Tweet;
 use App\Tweets\TweetType;
@@ -15,9 +18,13 @@ class TweetRetweetController extends Controller
             'type' => TweetType::RETWEET,
             'original_tweet_id' => $tweet->id
         ]);
+        broadcast(new TweetWasCreated($retweet));
+        broadcast(new TweetRetweetsWereUpdated($request->user(), $tweet));
     }
     public function destroy(Tweet $tweet, Request $request)
     {
+        broadcast(new TweetWasDeleted($tweet->retweetedTweet));
         $tweet->retweetedTweet()->where('user_id', $request->user()->id)->delete();
+        broadcast(new TweetRetweetsWereUpdated($request->user(), $tweet));
     }
 }
