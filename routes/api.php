cbb76d5ae67701c5b2fcdfa32663d18c5b2a86ce
bassel-mousa <--get>
2021-03-29 00:29:57 +0300

    Making A API Route

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::delete('/tweets/{tweet}/likes', [\App\Http\Controllers\Api\Tweets\TweetLi
 
 Route::post('/tweets/{tweet}/retweets', [\App\Http\Controllers\Api\Tweets\TweetRetweetController::class, 'store']);
 Route::delete('/tweets/{tweet}/retweets', [\App\Http\Controllers\Api\Tweets\TweetRetweetController::class, 'destroy']);
+
+Route::get('/media/types', [\App\Http\Controllers\Api\Media\MediaTypesController::class, 'index']);
