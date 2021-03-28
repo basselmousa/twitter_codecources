0209b41d824cc9af4382c0374a1bd780658a38b2
bassel-mousa <--get>
2021-03-29 00:29:32 +0300

    Making A MimeType Class

---
+++ app/Media/MimeTypes.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Media;
+
+
+class MimeTypes
+{
+    public static $image = [
+        'image/jpg',
+        'image/png',
+        'image/jpeg',
+    ];
+
+    public static $video = [
+        'video/mp4',
+    ];
+
+}
