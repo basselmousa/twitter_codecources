da5bdd701fd6ab35fa40e8c0dc5a82d5a6a6bd06
bassel-mousa <--get>
2021-04-02 02:02:14 +0300

    Making Function To Return Type

--- app/Media/MimeTypes.php
+++ app/Media/MimeTypes.php
@@ -21,4 +21,15 @@ class MimeTypes
         return array_merge(self::$image, self::$video);
     }
 
+    public static function type($mime)
+    {
+        if(in_array($mime, self::$image)){
+            return 'image';
+        }
+        if(in_array($mime, self::$video)){
+            return 'video';
+        }
+        return null;
+    }
+
 }
