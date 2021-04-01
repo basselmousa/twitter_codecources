189be913e9864ea4743d2fd3d0a5e68240c41381
bassel-mousa <--get>
2021-04-02 01:59:05 +0300

    Inhirit The Media Model to Make Our Methods

---
+++ app/Media/Media.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Media;
+use Spatie\MediaLibrary\Models\Media as BaseMedia;
+
+class Media extends BaseMedia
+{
+    public function type()
+    {
+        return MimeTypes::type($this->mime_type);
+    }
+}
